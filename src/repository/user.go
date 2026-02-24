edbf379dd1b49ee7ccd7422b2828525819d751d8
Tohru Yaginuma <toru45s@gmail.com>
2026-02-23 17:23:16 -0800

    Add database connection handling and user repository methods for creating, finding, and checking existence by Google ID

--- src/repository/user.go
+++ src/repository/user.go
@@ -1,19 +1,82 @@
 package repository
 
 import (
+	"context"
 	"database/sql"
+	"errors"
 
+	"github.com/jmoiron/sqlx"
 	"github.com/tohruyaginuma/TranscriptKeeper-Backend/src/domain"
 )
 
+var (
+	ErrUserNotFound = errors.New("user not found")
+)
+
 type UserRepository struct {
-	db *sql.DB
+	db *sqlx.DB
 }
 
-func NewUserRepository(db *sql.DB) *UserRepository {
+func NewUserRepository(db *sqlx.DB) *UserRepository {
 	return &UserRepository{db: db}
 }
 
-func (r *UserRepository) Create(user *domain.User) (*domain.User, error)                {}
-func (r *UserRepository) FindByGoogleID(googleID domain.GoogleID) (*domain.User, error) {}
-func (r *UserRepository) ExistsByGoogleID(googleID domain.GoogleID) (bool, error)       {}
+func (r *UserRepository) Create(ctx context.Context, googleID domain.GoogleID) (userID domain.UserID, err error) {
+	const query = `
+		INSERT INTO users (google_id)
+		VALUES ($1)
+		RETURNING id
+	;`
+
+	var id int64
+	err = r.db.GetContext(ctx, &id, query, googleID)
+	if err != nil {
+		return 0, err
+	}
+
+	userID, err = domain.NewUserID(id)
+	if err != nil {
+		return 0, err
+	}
+
+	return userID, nil
+}
+
+func (r *UserRepository) FindByGoogleID(ctx context.Context, googleID domain.GoogleID) (userID domain.UserID, err error) {
+	const query = `
+		SELECT id
+		FROM users 
+		WHERE google_id = $1
+	;`
+
+	var id int64
+	err = r.db.GetContext(ctx, &id, query, googleID)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return 0, ErrUserNotFound
+		}
+		return 0, err
+	}
+
+	userID, err = domain.NewUserID(id)
+	if err != nil {
+		return 0, err
+	}
+
+	return userID, nil
+}
+
+func (r *UserRepository) ExistsByGoogleID(ctx context.Context, googleID domain.GoogleID) (exists bool, err error) {
+	const query = `
+		SELECT EXISTS (
+			SELECT 1 FROM users WHERE google_id = $1
+		)
+	;`
+
+	err = r.db.GetContext(ctx, &exists, query, googleID)
+	if err != nil {
+		return false, err
+	}
+
+	return exists, nil
+}
